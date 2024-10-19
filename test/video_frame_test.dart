7dff70b6f002b915cdc42ce8a203ad92ffeee4e6
Miran Kamal <32594520+MiranKm@users.noreply.github.com>
2024-10-19 03:31:16 +0300

    more fixes merge conflicts

--- test/video_frame_test.dart
+++ test/video_frame_test.dart
@@ -1,6 +1,6 @@
+import 'package:export_video_frame/export_video_frame.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:video_frame/video_frame.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('video_frame');
@@ -17,7 +17,4 @@ void main() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await ExportVideoFrame.platformVersion, '42');
-  });
 }
